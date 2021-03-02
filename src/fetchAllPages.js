15a0c8a764e9fe022ce277fefe87bfd2372a97c5
cAttte <26514199+cAttte@users.noreply.github.com>
2021-03-01 22:02:23 -0300

    feat(fetchAllPages): log

--- src/fetchAllPages.js
+++ src/fetchAllPages.js
@@ -7,12 +7,14 @@ const cheerio = require("cheerio")
  * @returns {Promise<string[]>}
  */
 module.exports = async function fetchAllPages(url) {
+    console.log("Fetching page 1...")
     const firstPage = await fetch(url).then(res => res.text())
     const $ = cheerio.load(firstPage)
     const pageCount = Number($("div.pagination > a:last-child").text())
     const pages = [firstPage]
 
     for (let page = 2; page <= pageCount; page++) {
+        console.log(`Fetching page ${page}/${pageCount}...`)
         const body = await fetch(url + `?page=${page}`).then(res => res.text())
         pages.push(body)
     }
