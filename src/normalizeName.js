5238537437afd6afcd83900dd862a6e2966aa3da
cAttte <26514199+cAttte@users.noreply.github.com>
2021-03-01 21:11:57 -0300

    refactor(normalizeName): check for parens, |, and 'US[A]' (US-centric moment)

--- src/normalizeName.js
+++ src/normalizeName.js
@@ -4,8 +4,10 @@
  */
 module.exports = function normalizeName(name) {
     return name
-        .replace(/(BTE|Build ?The ?Earth)\s*(:|-)?\s*/i, "")
+        .replace(/(BTE|Build ?The ?Earth)\s*[:-|]?\s*/i, "")
         .replace(/\s*(Build)? ?Team\s*/i, "")
         .replace(/\s*1:1\s*/, "")
+        .replace(/\s*\(.+\)\s*/, "")
+        .replace(/\s*USA?^/, "")
         .toLowerCase()
 }
