2712ee5b696b6c1d449588e6448fa2c218772c19
cAttte <26514199+cAttte@users.noreply.github.com>
2021-03-01 21:34:01 -0300

    feat: create fetchAllPages()

---
+++ src/fetchAllPages.js
@@ -0,0 +1,21 @@
+/** @type {(url: RequestInfo, init?: RequestInit) => Promise<Response>} */
+const fetch = require("node-fetch")
+const cheerio = require("cheerio")
+
+/**
+ * @param {string} url
+ * @returns {string[]}
+ */
+module.exports = async function fetchAllPages(url) {
+    const firstPage = await fetch(url).then(res => res.text())
+    const $ = cheerio.load(firstPage)
+    const pageCount = Number($("div.pagination > a:last-child").text())
+    const pages = [firstPage]
+
+    for (let page = 2; page <= pageCount; page++) {
+        const body = await fetch(url + `?page=${page}`).then(res => res.text())
+        pages.push(body)
+    }
+
+    return pages
+}
