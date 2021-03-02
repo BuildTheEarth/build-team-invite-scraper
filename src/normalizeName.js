5a0e1f718ab7d492db16f3a5bbc2aac386f25a30
cAttte <26514199+cAttte@users.noreply.github.com>
2021-03-01 23:12:46 -0300

    fix(normalizeName): rearrange [] in regex to not form a range

--- src/normalizeName.js
+++ src/normalizeName.js
@@ -4,7 +4,7 @@
  */
 module.exports = function normalizeName(name) {
     return name
-        .replace(/(BTE|Build ?The ?Earth)\s*[:-|]?\s*/i, "")
+        .replace(/(BTE|Build ?The ?Earth)\s*[-:|]?\s*/i, "")
         .replace(/\s*(Build)? ?Team\s*/i, "")
         .replace(/\s*1:1\s*/, "")
         .replace(/\s*\(.+\)\s*/, "")
